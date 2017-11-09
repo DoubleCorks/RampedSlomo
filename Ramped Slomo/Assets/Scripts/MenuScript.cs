ebe7bd395f663ed73b68d4a9f66df23340ae2fa7
Michael Sorger <u1019336@utah.edu>
2017-11-09 01:55:25 -0700

    Added start scene with animated whiteBox menu and menu button. Added a menu manager GameObject to write scripts to

---
+++ Ramped Slomo/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MenuScript : MonoBehaviour
+{
+    public GameObject whiteBox;
+    private Animator anim;
+
+    public bool menuIsOpen = false;
+
+    void Start()
+    {
+        //get then disable animator on start to stop it from playing the default animation
+        anim = whiteBox.GetComponent<Animator>();
+        anim.enabled = false;
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) == true && menuIsOpen == true && isMouseOverWhiteBox() == false)
+        {
+            Debug.Log("You have clicked away the menu button!");
+            //enable the animator component
+            //play the Slidein animation
+            anim.Play("SlideWhiteBoxOut");
+            menuIsOpen = false;
+        }
+    }
+
+    public void OpenMenuOnClick()
+    {
+        Debug.Log("You have clicked open the menu button!");
+        //enable the animator component
+        anim.enabled = true;
+        //play the Slidein animation
+        anim.Play("SlideWhiteBoxIn");
+        menuIsOpen = true;
+    }
+
+    public bool isMouseOverWhiteBox()
+    {
+        Vector2 mousePosition = Input.mousePosition;
+        
+        if (mousePosition.x >= 0 && mousePosition.x < 220
+           && mousePosition.y >= 0 && mousePosition.y < 480)
+        {
+            Debug.Log("yessir, in the box");
+            Debug.Log(Input.mousePosition);
+            return true;
+        }
+        Debug.Log("not in the box");
+        Debug.Log(Input.mousePosition);
+        return false;
+    }    
+
+}
+
